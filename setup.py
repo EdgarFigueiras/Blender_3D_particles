9483d913082e81b6e8431de8ea67394c3ca7e1c0
EdgarFigueiras <figueirasgomez@gmail.com>
2018-07-16 22:27:04 +0200

    V1.0
    Complete install method for cArray

--- setup.py
+++ setup.py
@@ -3,10 +3,10 @@ import numpy.distutils.misc_util
 
 
 setup (name = 'cArray',
-        version = '1.0',
-        description = 'This is a package for improving matrix calculations',
-        ext_modules=[
-       		Extension('cArray', sources = ['cArray.c'],
-                include_dirs=[numpy.get_include()]),
-    ],
-)
+       version = '1.0',
+       description = 'This is a package for improving matrix calculations',
+       ext_modules=[
+                    Extension('cArray', sources = ['cArray.c'],
+                              include_dirs=[numpy.get_include()]),
+                    ],
+       )
